8e623f9b6be996087f99c5536852e8598090fdd7
SharkEzz <icraft640@gmail.com>
2019-02-02 18:30:13 +0100

    Added back nand module
    Also put all modules in one folder for better visibility

--- device.mk
+++ device.mk
@@ -14,6 +14,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel \
-    $(LOCAL_PATH)/recovery/root/ft5x_ts.ko:recovery/root/ft5x_ts.ko \
+    $(LOCAL_PATH)/recovery/root/modules/ft5x_ts.ko:recovery/root/ft5x_ts.ko \
+    $(LOCAL_PATH)/recovery/root/modules/goodix_touch.ko:recovery/root/goodix_touch.ko \
+    $(LOCAL_PATH)/recovery/root/modules/nand.ko:recovery/root/nand.ko
 
 $(call inherit-product-if-exists, build/target/product/full.mk)
