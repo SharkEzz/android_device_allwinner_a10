e48eeb34ca6b500ceae2c571a440141545ae5197
SharkEzz <icraft640@gmail.com>
2019-02-02 19:59:34 +0100

    Replace modules with latest one and added sun4i-ts module

--- device.mk
+++ device.mk
@@ -16,6 +16,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel \
     $(LOCAL_PATH)/recovery/root/modules/ft5x_ts.ko:recovery/root/ft5x_ts.ko \
     $(LOCAL_PATH)/recovery/root/modules/goodix_touch.ko:recovery/root/goodix_touch.ko \
+    $(LOCAL_PATH)/recovery/root/modules/sun4i-ts.ko:recovery/root/sun4i-ts.ko \
     $(LOCAL_PATH)/recovery/root/modules/nand.ko:recovery/root/nand.ko
 
 $(call inherit-product-if-exists, build/target/product/full.mk)
